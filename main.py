ddf5172c8c7707741d8706eeb3b586b45a850415
Sune Lauth Gadegaard <sunegadegaard@gmail.com>
2024-09-11 16:02:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ if "__main__" == __name__:
     bnb.setNodeSelectionStrategy('bestBound')
     # Options are {"mostOftenFractional", "averageMostFractional", "random"}
     bnb.setBranchingStrategy('mostOftenFractional')
+    # Options are {'LPRelaxation', 'IdealPoint'}
+    bnb.setLowerBoundStrategy('LPRelaxation')
     # Un-comment this line if you want to load an optimal solution before solving (mimics a strong heuristic)
     # bnb.loadSolutionsFromFile('instances/instance_10_3_solution.json')
     # Un-comment the following line if you want to inherit the lower bound set from the parent node
